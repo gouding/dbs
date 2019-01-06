38f762786986d3b4f7f18125d7a46f54dc7e90b6
denzel <422293027@qq.com>
2019-01-06 14:28:45 +0800

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('index')
+console.log('ddd')
+const fetchData = () => {
+  fetch('http://47.52.229.225:3000/user', {
+    method: 'GET',
+    mode: 'cors',
+    // credentials:'omit'
+  }).then(res => res.text())
+    .then(res => {
+      document.getElementById('root').innerHTML=res;
+    })
+}
+
+fetchData();
