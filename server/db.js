ed5c35d8f54865ba47e63627b57b6f4457493e17
denzel <422293027@qq.com>
2019-01-07 16:29:52 +0800

    dev

--- server/db.js
+++ server/db.js
@@ -10,32 +10,34 @@
 // module.exports = mongoose;
 
 var mongoose = require('mongoose'),
-    DB_URL = 'mongodb://47.52.229.225:27017/test';
+    DB_URL = 'mongodb://127.0.0.1:27017/test';
 
 /**
  * 连接
  */
-mongoose.connect(DB_URL);
+mongoose.connect(DB_URL, {
+    useNewUrlParser:true
+});
 
 /**
   * 连接成功
   */
-mongoose.connection.on('connected', function () {    
-    console.log('Mongoose connection open to ' + DB_URL);  
-});    
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connection open to ' + DB_URL);
+});
 
 /**
  * 连接异常
  */
-mongoose.connection.on('error',function (err) {    
-    console.log('Mongoose connection error: ' + err);  
-});    
- 
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
 /**
  * 连接断开
  */
-mongoose.connection.on('disconnected', function () {    
-    console.log('Mongoose connection disconnected');  
-}); 
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose connection disconnected');
+});
 
 module.exports = mongoose;
